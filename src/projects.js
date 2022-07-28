44c6b4b28c90ae0bfb0fe0ef4da2710d9ad33b86
Yohn Ramirez <yohnramirezsilva7@gmail.com>
2022-07-28 17:23:45 -0500

    add rick and morty project

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,19 @@
 const projects = [
     {
+        "title": "Rick and Morty App: React JS + Chakra UI",
+        "description": "In this project, the Rick and Morty API was used to create and search for any character from the series by means of cards.",
+        "preview": "/images/r-a-m.png",
+        "repository": "https://github.com/yohnramirez/rick---morty-reactjs",
+        "web": "https://rick-and-morty-react-api.netlify.app/"
+    },
+    {
+        "title": "Generic store: Fullstack Project",
+        "description": "This frontend is part of a fullstack project; made with React JS and Bootstrap 4.",
+        "preview": "/images/tienda.png",
+        "repository": "https://github.com/yohnramirez/interfaces",
+        "web": "https://tienda-generica.netlify.app"
+    },
+    {
         "title": "Black & White Gallery",
         "description": "Website inspired by a black and white photo gallery made with html, css and vanilla javascript.",
         "preview": "/images/black-gallery.png",
@@ -14,20 +28,12 @@ const projects = [
         "web": "https://scamper.netlify.app"
     },
     {
-        "title": "Generic store: Fullstack Project",
-        "description": "This frontend is part of a fullstack project; made with React JS and Bootstrap 4.",
-        "preview": "/images/tienda.png",
-        "repository": "https://github.com/yohnramirez/interfaces",
-        "web": "https://tienda-generica.netlify.app"
-    },
-    {
         "title": "Skin Rules: Landing Page",
         "description": "Use of an HTML template for the creation of a landing page for a Colombian clothing brand. No framework was used.",
         "preview": "/images/skin.png",
         "repository": "",
         "web": "https://skinrules.netlify.app"
     },
-
 ]
 
 export default projects
